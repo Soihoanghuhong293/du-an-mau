d1f0a3ec24219c8d01174d190daffe170125cf09
Soihoanghuhong293 <pxq293it@gmail.com>
2025-08-16 00:39:07 +0700

    sfd

---
+++ admin/header.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>admin</title>
+    <link rel="stylesheet" href="../css/style.css">
+</head>
+<body>
+    <div class="boxcenter">
+        <div class="row mb headeradmin ">
+                    <h1> admin</h1>
+        </div>
+        <div class="row mb menu">
+            <ul>
+                <li><a href="#">Trang chủ</a></li>
+                <li><a href="#">danh muc</a></li>
+                <li><a href="#">hang hoa </a></li>
+                <li><a href="#">khach hang</a></li>
+                <li><a href="#">binh luan</a></li>
+                <li><a href="#">thong ke</a></li>
+            </ul>
+        </div>
---
+++ admin/home.php
@@ -0,0 +1,24 @@
+<div class="row">
+            <div class="row frmtitle"><h1>them moi loai hang hoa</h1></div>
+            <div class="row frmcontent">
+                <form action="#" method="post">
+                    <div class="row mb10">
+                        ma loai <br>
+                    <input type="text" name="maloai" disabled>
+
+                    </div>
+                     <div class="row mb10">
+                    ten loai <br>
+                    <input type="text" name="tenloai">
+                    </div>
+                    <div class="row mb10">
+                        <input type="submit" value="Them moi">
+                        <input type="reset" value="nhap lai">
+                        <a href="danhsach_loaihang.html">
+                        <input type="button" value="danh sasch"></a>
+                        
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
